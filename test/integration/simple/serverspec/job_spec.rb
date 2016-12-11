5562ddf63e0d44ca4b797faafd1a85afd5af4ed2
Adham Helal <aa@hellofresh.com>
2016-12-11 10:29:13 +0100

    wait for started jobs

--- test/integration/simple/serverspec/job_spec.rb
+++ test/integration/simple/serverspec/job_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../../helper_spec.rb'
 
+describe command('while /usr/local/bin/fly -t kitchen builds | grep started > /dev/null; do echo "jobs still running"; sleep 2; done') do
+  it 'No pending jobs' do
+    expect(subject.exit_status).to eq(0)
+  end
+end
+
 describe command('/usr/local/bin/fly -t kitchen builds | grep succeeded | wc -l') do
   it 'some concourse pipelines should be succeeded' do
     expect(subject.stdout).to match('2')
