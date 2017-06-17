ceab4b20a7c1ff63a4c6038900de3bb0c1981345
Adham Helal <aa@hellofresh.com>
2017-06-17 23:43:27 +0200

    fix setup

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-#!/bin/sh
-set -e
-
 AVM_VERSION="v1.0.0-rc.8"
-
 export ANSIBLE_VERSIONS_0="2.0.2.0"
 export INSTALL_TYPE_0="pip"
 export ANSIBLE_LABEL_0="v2.0"
@@ -24,7 +20,7 @@ export ANSIBLE_DEFAULT_VERSION="v2.0"
 ## Create a temp dir to download avm
 avm_dir="$(mktemp -d 2> /dev/null || mktemp -d -t 'mytmpdir')"
 git clone https://github.com/ahelal/avm.git "${avm_dir}" > /dev/null 2>&1
-git checkout "${AVM_VERSION}"
+git checkout ${AVM_VERSION}
 /bin/sh "${avm_dir}"/setup.sh
 
 exit 0
