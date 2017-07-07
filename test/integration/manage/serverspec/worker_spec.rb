b5f649bc1d20d86c3390cfdad789a8719c5c8d8c
Adham <ahelal@users.noreply.github.com>
2017-07-07 13:07:37 +0200

    Feature/support all (#31)
    * some cleanup
    
    * New config yet
    
    * more fixes
    
    * remove manage
    
    * upgrade of serverspec
    
    * fix credentials and tests
    
    * documentation update

--- test/integration/manage/serverspec/worker_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../helper_spec.rb'
-
-describe command('/usr/local/bin/fly -t kitchen workers | wc -l') do
-  it 'concourse worker should be registered' do
-    expect(subject.stdout).to match('1')
-    expect(subject.exit_status).to eq(0)
-  end
-end
