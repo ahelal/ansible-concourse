c061a78e85bc6fe4bde6379f5f78c2f7cd2d37bd
Adham <ahelal@users.noreply.github.com>
2017-11-08 10:06:43 +0100

    Upgrade to new postgresql role for tests

---
+++ test/setup_roles.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+set -e
+MY_PATH="$(dirname "${0}")"
+DIR="$( cd "${MY_PATH}" && pwd )"  # absolutized and normalized
+ROLES_DIR=$(cd "${DIR}/helper_roles" && pwd)
+
+ansible-galaxy install -r "${ROLES_DIR}/roles_requirements.yml" --force -p "${ROLES_DIR}"
