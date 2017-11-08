169eebc13b93621e6c36e6deb326d260084b30de
Adham <ahelal@users.noreply.github.com>
2017-11-08 20:59:27 +0100

    upgrade 3.6 (#53)
    Upgrade 3.6
    Increase the matrix of Ansible testing
    Replace the standard postgreSQL role

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 set -e
 
-AVM_VERSION="v1.0.0-rc.8"
+AVM_VERSION="v1.0.0"
 export ANSIBLE_VERSIONS_0="2.0.2.0"
 export INSTALL_TYPE_0="pip"
 export ANSIBLE_LABEL_0="v2.0"
 
-export ANSIBLE_VERSIONS_1="2.2.1.0"
+export ANSIBLE_VERSIONS_1="2.1.6.0"
 export INSTALL_TYPE_1="pip"
-export ANSIBLE_LABEL_1="v2.2"
+export ANSIBLE_LABEL_1="v2.1"
 
-export ANSIBLE_VERSIONS_2="2.3.1.0"
+export ANSIBLE_VERSIONS_2="2.2.3.0"
 export INSTALL_TYPE_2="pip"
-export ANSIBLE_LABEL_2="v2.3"
+export ANSIBLE_LABEL_2="v2.2"
+
+export ANSIBLE_VERSIONS_3="2.3.2.0"
+export INSTALL_TYPE_3="pip"
+export ANSIBLE_LABEL_3="v2.3"
+
+export ANSIBLE_VERSIONS_4="2.4.1.0"
+export INSTALL_TYPE_4="pip"
+export ANSIBLE_LABEL_4="v2.4"
 
 # Whats the default version
 export ANSIBLE_DEFAULT_VERSION="v2.0"
