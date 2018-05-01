279651ca05931a7a6eb8d6e34d7e9c1a03276426
Adham <ahelal@users.noreply.github.com>
2018-05-01 11:38:37 +0200

    Test fix (#66)
    * use last 3 version of ansible only
    
    * upgrade ansible push and fix kitchen.yml
    
    * fix default version
    
    * fix avm path
    
    * change order of tests
    
    * debug travis
    
    * Fix fly version issue

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -2,28 +2,21 @@
 set -e
 
 AVM_VERSION="v1.0.0"
-export ANSIBLE_VERSIONS_0="2.0.2.0"
-export INSTALL_TYPE_0="pip"
-export ANSIBLE_LABEL_0="v2.0"
 
-export ANSIBLE_VERSIONS_1="2.1.6.0"
+export ANSIBLE_VERSIONS_1="2.3.3.0"
 export INSTALL_TYPE_1="pip"
-export ANSIBLE_LABEL_1="v2.1"
+export ANSIBLE_LABEL_1="v2.3"
 
-export ANSIBLE_VERSIONS_2="2.2.3.0"
+export ANSIBLE_VERSIONS_2="2.4.4.0"
 export INSTALL_TYPE_2="pip"
-export ANSIBLE_LABEL_2="v2.2"
+export ANSIBLE_LABEL_2="v2.4"
 
-export ANSIBLE_VERSIONS_3="2.3.2.0"
+export ANSIBLE_VERSIONS_3="2.5.2"
 export INSTALL_TYPE_3="pip"
-export ANSIBLE_LABEL_3="v2.3"
-
-export ANSIBLE_VERSIONS_4="2.4.2.0"
-export INSTALL_TYPE_4="pip"
-export ANSIBLE_LABEL_4="v2.4"
+export ANSIBLE_LABEL_3="v2.5"
 
 # Whats the default version
-export ANSIBLE_DEFAULT_VERSION="v2.0"
+export ANSIBLE_DEFAULT_VERSION="v2.3"
 
 ## Create a temp dir to download avm
 avm_dir="$(mktemp -d 2> /dev/null || mktemp -d -t 'mytmpdir')"
