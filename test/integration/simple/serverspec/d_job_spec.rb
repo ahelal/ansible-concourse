241b02a848b472bb3851990e8de1cef44c739f47
Adham <ahelal@users.noreply.github.com>
2018-05-01 10:54:16 +0200

    debug travis

--- test/integration/simple/serverspec/d_job_spec.rb
+++ test/integration/simple/serverspec/d_job_spec.rb
@@ -3,23 +3,35 @@ require_relative '../../helper_spec.rb'
 fly = '/usr/local/bin/fly -t kitchen'
 builds = "#{fly} builds"
 
+
+
+
 describe 'simple_success job' do
   describe command("#{builds} | grep simple_success") do
     it 'simple_success are in the builds' do
+        puts "#################################"
+        puts  "#{subject.stdout}"
+        puts "#################################"
       expect(subject.stdout).to match('simple_success')
       expect(subject.exit_status).to eq(0)
     end
   end
 
   describe command("#{builds} | grep simple_success | head -1 | awk '{ print $4 }'") do
-    it 'simple_success pipelines should be succeeded' do
+    it 'simple_success pipeline should succeed' do
+        puts "#################################"
+        puts  "#{subject.stdout}"
+        puts "#################################"
       expect(subject.stdout).to match('succeeded')
       expect(subject.exit_status).to eq(0)
     end
   end
 
   describe command("#{fly} watch -b $(#{builds} | grep simple_success | head -1 | awk '{ print $1 }')") do
-    it 'simple_success pipelines log should have "I WILL SUCCESSED"' do
+    it 'simple_success pipeline log should have "I WILL SUCCESSED"' do
+        puts "#################################"
+        puts  "#{subject.stdout}"
+        puts "#################################"
       expect(subject.stdout).to match('I WILL SUCCESSED')
       expect(subject.exit_status).to eq(0)
     end
