e51ad092aeda81061dfd4a9602db6c828fad2aa6
Adham <ahelal@users.noreply.github.com>
2019-05-10 09:11:57 +0200

    setup different version

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -3,24 +3,29 @@ set -e
 
 AVM_VERSION="v1.0.0"
 
-export ANSIBLE_VERSIONS_1="2.5.13"
+export ANSIBLE_VERSIONS_1="2.5.15"
 export INSTALL_TYPE_1="pip"
-export ANSIBLE_LABEL_1="v2.5"
+export ANSIBLE_LABEL_1="vOne"
 
-export ANSIBLE_VERSIONS_2="2.6.10"
+export ANSIBLE_VERSIONS_2="2.6.16"
 export INSTALL_TYPE_2="pip"
-export ANSIBLE_LABEL_2="v2.6"
+export ANSIBLE_LABEL_2="vTwo"
 
-export ANSIBLE_VERSIONS_3="2.7.4"
+export ANSIBLE_VERSIONS_3="2.7.10"
 export INSTALL_TYPE_3="pip"
-export ANSIBLE_LABEL_3="v2.7"
+export ANSIBLE_LABEL_3="vThree"
 
 # Whats the default version
 export ANSIBLE_DEFAULT_VERSION="v2.5"
 
+echo "* Setting up ansible "
+echo "* ANSIBLE_LABEL_1=${ANSIBLE_LABEL_1} ANSIBLE_VERSIONS_1=${ANSIBLE_VERSIONS_1}"
+echo "* ANSIBLE_LABEL_2=${ANSIBLE_LABEL_2} ANSIBLE_VERSIONS_2=${ANSIBLE_VERSIONS_2}"
+echo "* ANSIBLE_LABEL_3=${ANSIBLE_LABEL_3} ANSIBLE_VERSIONS_3=${ANSIBLE_VERSIONS_3}"
+
 ## Create a temp dir to download avm
-avm_dir="$(mktemp -d 2> /dev/null || mktemp -d -t 'mytmpdir')"
-git clone https://github.com/ahelal/avm.git "${avm_dir}" > /dev/null 2>&1
+avm_dir="$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')"
+git clone https://github.com/ahelal/avm.git "${avm_dir}" >/dev/null 2>&1
 cd "${avm_dir}"
 git checkout ${AVM_VERSION}
 /bin/sh "${avm_dir}"/setup.sh
--- test/setup_roles.sh
+++ test/setup_roles.sh
@@ -1,7 +1,7 @@
- #!/bin/bash
+#!/bin/bash
 set -e
 MY_PATH="$(dirname "${0}")"
-DIR="$( cd "${MY_PATH}" && pwd )"  # absolutized and normalized
+DIR="$(cd "${MY_PATH}" && pwd)" # absolutized and normalized
 ROLES_DIR=$(cd "${DIR}/helper_roles" && pwd)
 
 ansible-galaxy install -r "${ROLES_DIR}/roles_requirements.yml" --force -p "${ROLES_DIR}"
